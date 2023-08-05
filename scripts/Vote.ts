b3c8b1d4acac5f9f8efc8c1ec003aaa2ab8a8c6c
armsves <armsves@gmail.com>
2023-08-05 10:36:46 +0200

    first commit

---
+++ scripts/Vote.ts
@@ -0,0 +1,33 @@
+import { ethers } from "ethers";
+import { Ballot__factory } from "../typechain-types";
+import { Ballot } from "../typechain-types";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+async function main() {
+    const parameter = process.argv.slice(2);
+    const contractAddress = parameter[0];
+    const castVote = parameter[1];
+
+const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL ?? "");
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
+
+console.log(`Using address ${wallet.address}`);
+const balanceBN = await provider.getBalance(wallet.address);
+const balance = Number(ethers.formatUnits(balanceBN));
+console.log(`Wallet balance ${balance}`);
+if (balance < 0.01) {
+  throw new Error("Not enough ether");
+}
+
+const ballotFactory = new Ballot__factory(wallet);
+const ballotContract = ballotFactory.attach(contractAddress) as Ballot;
+
+const tx = await ballotContract.vote(castVote);
+
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ tests/Ballot.ts
@@ -0,0 +1,10 @@
+import { ethers} from "hardhat";
+
+ethers.getContractFactory("Ballot");
+
+describe("Ballot", async () => {
+    describe("When the contract is deployed", async () => {
+        it("has provided proposals", async () => { });
+    })
+
+})
